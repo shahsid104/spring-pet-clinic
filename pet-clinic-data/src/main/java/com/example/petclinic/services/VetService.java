133edc0aa9c7b5c8e7386bbe6c89ea76917c78cf
Sid Shah <sid.shah@amobee.com>
2021-03-30 01:05:48 -0700

    Adding new services

--- pet-clinic-data/src/main/java/com/example/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/VetService.java
@@ -5,10 +5,6 @@ import com.example.petclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
+public interface VetService extends CrudService<Vet, Long> {
 
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
